543a147b5b35032425381c88b3ec1cf62780f0fe
Marti-Win7 <Marti-Win7@users.noreply.github.com>
2018-02-15 13:02:03 +0100

    Add files via upload

--- js/sidebar-nav.js
+++ js/sidebar-nav.js
@@ -23,20 +23,22 @@ var data =
         { href: "einstellungen.htm#gui", title: "GUI"},
         { href: "einstellungen.htm#log", title: "Log"},
     ]},
-    {href: "serienplaner.htm", title: "Serien-Planer", submenus: [
+    {href: "serienplaner.htm", title: "SerienPlaner", submenus: [
         { href: "serienplaner.htm#anzeigemodus", title: "Tagesübersicht / TOP30"},
         { href: "serienplaner.htm#sender-zuordnen", title: "Sender zuordnen"},
         { href: "serienplaner.htm#serien-marker", title: "Serien-Marker"},
         { href: "serienplaner.htm#timer-liste", title: "Timer-Liste"},
         { href: "serienplaner.htm#marker-hinzufuegen", title: "Marker hinzufügen"},
     ]},
-	{href: "serienmarker.htm", title: "Serien-Marker", submenus: [
-        { href: "serienplaner.htm#anzeigemodus", title: "Anzeige-Modus"},
-        { href: "serienplaner.htm#sender-zuordnen", title: "Sender zuordnen"},
-        { href: "serienplaner.htm#serien-marker", title: "Serien-Marker"},
-        { href: "serienplaner.htm#timer-liste", title: "Timer-Liste"},
-        { href: "serienplaner.htm#marker-hinzufuegen", title: "Marker hinzufügen"},
+	{href: "serienmarker.htm", title: "SerienMarker", submenus: [
+        { href: "serienmarker.htm#deaktivieren", title: "(De) aktivieren/löschen"},
+        { href: "serienmarker.htm#sender-auswaehlen", title: "Sender auswählen"},
+        { href: "serienmarker.htm#staffel-auswaehlen", title: "Staffel auswählen"},
+        { href: "serienmarker.htm#sendetermine", title: "Sendetermine"},
+        { href: "serienmarker.htm#timer-liste", title: "Timer-Liste"},
     ]},
+	{href: "serie-hinzufügen.htm", title: "Serie/Marker hinzufügen",},
+	{href: "timer-liste.htm", title: "Timer-Liste",},
 	{href: "faq.htm", title: "FAQ",},
 ]};
 var html = template(data);
